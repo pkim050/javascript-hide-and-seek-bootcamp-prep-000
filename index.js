0e642b7ab5083b83a8de9fc0519787e31f1de098
Patrick A Kim <pkim050@gmail.com>
2018-11-25 19:13:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  const lis = document.querySelectorAll('#grand-node div');
+  for (var i = 0; i < lis.length; i++) {
+    if (i + 1 === lis.length) {
+      return lis[i];
+    }
+  }
+}
+
+function increaseRankBy(n) {
+  const parent = document.querySelectorAll('.ranked-list li');
+  for (var j = 0; j < parent.length; j++) {
+    parent[j].innerHTML = parseInt(parent[j].innerHTML) + n;
+  }
+}
