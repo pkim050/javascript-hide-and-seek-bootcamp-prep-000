8fe4c9453fde8644b9400c05d664dfd6ff664514
Patrick A Kim <pkim050@gmail.com>
2018-11-25 18:46:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   const parent = document.getElementsByClassName('ranked-list')[1];
-  const parent2 = document.querySelectorAll(.ranked-list li);
+  const parent2 = document.querySelectorAll('.ranked-list li');
   console.log(parent.innerHTML);
   console.log(parent2.innerHTML);
   const child = parent.children[1];
