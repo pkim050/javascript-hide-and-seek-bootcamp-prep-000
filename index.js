fa163d0e21c8094e1e671e93f62b3d637fbc0b42
Patrick A Kim <pkim050@gmail.com>
2018-11-25 17:37:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function increaseRankBy(n) {
   const parent2 = document.getElementsByClassName('ranked-list')[0];
   const child = parent.children[1];
   const child2 = parent.children[2];
-  console.log(parent.html);
+  console.log(parent.innerHTML);
   for (var j = 0; j < document.querySelectorAll('.ranked-list').length; j++) {
     
   }
