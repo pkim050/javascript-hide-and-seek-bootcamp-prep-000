d01922914dc5629879fd4d5d5e7f8d191221ff8f
Patrick A Kim <pkim050@gmail.com>
2018-11-25 18:47:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function nestedTarget() {
 
 function deepestChild() {
   const lis = document.querySelectorAll('#grand-node div');
-  console.log(lis.innerHTML);
   for (var i = 0; i < lis.length; i++) {
     if (i + 1 === lis.length) {
       return lis[i];
@@ -19,8 +18,7 @@ function deepestChild() {
 function increaseRankBy(n) {
   const parent = document.getElementsByClassName('ranked-list')[1];
   const parent2 = document.querySelectorAll('.ranked-list li');
-  console.log(parent.innerHTML);
-  console.log(parent2.innerHTML);
+  console.log(parent2.length);
   const child = parent.children[1];
   const child2 = parent.children[2];
   for (var j = 0; j < document.querySelectorAll('.ranked-list').length; j++) {
